37d05a1c40579b3ff0d6e8b4505881eef019d974
Vincent Chau <vichau@ucdavis.edu>
2019-02-24 17:34:04 -0800

    Initial commit

---
+++ LeakyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeakyApp
+//
+//  Created by Vincent Chau on 2/24/19.
+//  Copyright © 2019 Vincent Chau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
